46e0189a107430893b407ee4fed836ffd986becf
Fedor <fivashchenko@yandex.ru>
2022-12-23 23:34:35 +0300

    2nd stage

--- gittest.py
+++ gittest.py
@@ -1,6 +1,7 @@
 import functools
 import time
 
+#   timer decorator
 def timer(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -10,14 +11,25 @@ def timer(func):
         return res
     return wrapper
 
+#   factorial calculation using recursion
 def factorial(n):
     if n == 1:
         return 1
-    else:
-        return n * factorial(n-1)
+    return n * factorial(n-1)
 
+#   fibonachi number calculation using recursion
+def fibbo(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a = fibbo(n-1)
+    b = fibbo(n-2)
+    return a+b
+
+#   performing function
 @timer
 def perform(n):
-    return factorial(n)
+    return factorial(n), fibbo(n)
 
-print(perform(10))
+print(perform(30))
